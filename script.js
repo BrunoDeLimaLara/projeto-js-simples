75da5047dc2f1933801b405f62e837ac6a003397
Bruno de Lima Lara <112946544+BrunoDeLimaLara@users.noreply.github.com>
2024-10-02 23:39:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function exibirMensagem() {
     document.getElementById('resultado').innerHTML = 'Bem-vindo ao Projeto JS!';
 }
+// Desenvolvedor 2 - calcula a soma de dois números
+function somarNumeros(num1, num2) {
+    const soma = num1 + num2;
+    document.getElementById('resultado').innerHTML = `A soma é: ${soma}`;
+}
